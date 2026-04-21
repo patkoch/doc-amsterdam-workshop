1db90f895b94caa5469f171e877ca3d51365799e
Patrick Koch <patkoch87@gmail.com>
2026-04-21 12:50:13 +0000

    Add providers.tf

--- terraform-repo/variables.tf
+++ terraform-repo/variables.tf
@@ -7,10 +7,26 @@ variable "repository_description" {
   description = "A description for the DevOpsCon Terraform workshop repository"
   type        = string
   #default     = "An example repository created with Terraform" 
+
+  validation {
+    condition     = length(var.repository_description) > 0
+    error_message = "The repository description cannot be empty."
+  }
 }
 
-variable "repository_private" {
-  description = "Whether the repository should be private or public"
-  type        = bool
-  default     = false
+variable "github_owner" {
+  description = "The GitHub username or organization that will own the repository"
+  type        = string
+}
+
+variable "github_token" {
+  description = "A GitHub personal access token with permissions to create repositories"
+  type        = string
+  sensitive   = true
+}
+
+variable "visibility" {
+  description = "The visibility of the repository (public or private)"
+  type        = string
+  default     = "public"
 }
